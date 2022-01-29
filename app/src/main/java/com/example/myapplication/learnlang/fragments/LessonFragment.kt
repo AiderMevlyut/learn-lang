0b154e570e6233e0b3573e09a5c4b811cf345029
Ayder <mevlut.a.i.2.16@gmail.com>
2022-01-29 20:25:56 +0300

    Подключили Firabase. Подтягиваем данные

--- app/src/main/java/com/example/myapplication/learnlang/fragments/LessonFragment.kt
+++ app/src/main/java/com/example/myapplication/learnlang/fragments/LessonFragment.kt
@@ -1,18 +1,53 @@
 package com.example.myapplication.learnlang.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.myapplication.learnlang.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class LessonFragment : Fragment() {
+
+    val TAG: String = "MY DATA :::\t"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // Write a message to the database
+        // Write a message to the database
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("array")
+
+       // myRef.setValue("Hello, World!!!!!!!!!!!")
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.value
+                Log.d(TAG, "Value is: $value")
+
+                for (postSnapshot in dataSnapshot.children) {
+                    Log.d(TAG, "Value is: $postSnapshot")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lesson, container, false)
     }
